c06a07ac4f3cbafde05f160978e5015892bcd731
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:49:42 +0300

    Add files via upload

---
+++ KarasimsekDevresiYapimi.ino
@@ -0,0 +1,29 @@
+int beklemeSuresi = 50;
+
+void setup() {
+  
+  for(int i = 6; i<14;i++){
+    
+    pinMode(i,OUTPUT);
+  }
+}
+void loop() {
+  
+  for(int i = 6; i<14;i++){
+    
+    digitalWrite(i,HIGH);
+    delay(beklemeSuresi);
+    digitalWrite(i,LOW);
+    delay(beklemeSuresi);
+  }
+  
+  //Ters tarafa
+  
+  for(int i = 12; i>6;i--){
+    
+    digitalWrite(i,HIGH);
+    delay(beklemeSuresi);
+    digitalWrite(i,LOW);
+    delay(beklemeSuresi);
+  }
+}
